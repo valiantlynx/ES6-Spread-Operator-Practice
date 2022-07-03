4a8fcf864c9be4460975b6d7a962cea709556738
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2022-07-03 20:17:46 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const { value } = event.target;
+
+    setItem(value);
+  }
+
+  function handleClick() {
+    console.log(items);
+    setItems((prevValue) => {
+      return [...prevValue, item];
+    });
+    setItem("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={item} />
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((toDoItem) => (
+            <li> {toDoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
